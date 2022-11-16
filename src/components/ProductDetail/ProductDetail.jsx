28d9441652ccd92cbd4239f32544f8134af955bb
Berikbol <nn.z.icon@gmail.com>
2022-11-16 18:46:37 +0600

    chat send

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -4,22 +4,39 @@ import axios from 'axios';
 import { Carousel } from 'antd';
 import style from './ProductDetail.module.css'
 import Button from '../Button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
 
 export default function ProductDetail() {
     const {id} = useParams()
     const [data, setData] = useState()
+    const { tg } = useTelegram()
+    const [color, setColor] = useState()
+    const [memory, setMemory] = useState()
 
     useEffect(()=>{
         axios.get(`https://tg-backend-database.herokuapp.com/api/device/${id}`)
         .then(res => {
             setData(res.data);
-            console.log(res.data);
         })
         
     },[])
     useEffect(()=>{
         data ? document.getElementById('html').innerHTML = data.html : null
+        data ? document.querySelector('input[name=color]').checked = true : null
+        data ? document.querySelector('input[name=memory]').checked = true : null
     }, [data])
+
+
+    const sendData = () => {
+        const device = {
+            name: data.name,
+            price: data.price,
+            color: color,
+            memory: memory
+        }
+        tg.sendData(JSON.stringify(device))
+    }
+
   return data ? 
   <div className={style.product__detail}>
       <div className={style.image}>
@@ -29,7 +46,7 @@ export default function ProductDetail() {
         )}  
         </Carousel>
       </div>
-      <div className={style.discription}>
+      <form className={style.discription}>
           <h1>{data.name}</h1>
           <h2 className={style.price}>Цена: {data.price} KZT</h2>
           <div className={style.props}>
@@ -37,7 +54,7 @@ export default function ProductDetail() {
             <div className={style.variations}>
                 {JSON.parse(data.variations).color.map(e => 
                     <div className={style.color} style={{background: e}}>
-                        <input name='color' id={e} type="radio" />
+                        <input onChange={(e)=>setColor(e.target.value)} value={e} name='color' id={e} type="radio" />
                         <label for={e}></label>
                     </div>    
                 )}
@@ -46,17 +63,18 @@ export default function ProductDetail() {
             <div className={style.variations}>
                 {JSON.parse(data.variations).memory.map(e => 
                     <div className={style.memory}>
-                        <input name='memory' id={e} type="radio" />
+                        <input onChange={(e)=>setMemory(e.target.value)} value={e} name='memory' id={e} type="radio" />
                         <label for={e}>{e}</label>
                     </div>   
                 )}
             </div>
           </div>
           
-          <Button style={{display: 'flex', margin: '20px 0', marginLeft: 'auto'}}>Купить</Button>
+          <Button onClick={sendData} style={{display: 'flex', margin: '20px 0', marginLeft: 'auto'}}>Купить</Button>
+          <h1>Which {data.name.split(' ')[0]} is right for you?</h1>
           <div id="html">
           </div>
-      </div>
+      </form>
   </div>
 : null
 }
